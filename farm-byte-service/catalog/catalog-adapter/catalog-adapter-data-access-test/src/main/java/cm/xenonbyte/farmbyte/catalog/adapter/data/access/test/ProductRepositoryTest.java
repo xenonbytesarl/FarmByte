c3887d69c36eff8d47a4927f715d28fbbe66f792
Brice MBIANDJI <ambiandji@gmail.com>
2024-08-23 16:24:36 +0200

    FB-7 | Service | Implement test for repository in memory test for create product

---
+++ farm-byte-service/catalog/catalog-adapter/catalog-adapter-data-access-test/src/main/java/cm/xenonbyte/farmbyte/catalog/adapter/data/access/test/ProductRepositoryTest.java
@@ -0,0 +1,64 @@
+package cm.xenonbyte.farmbyte.catalog.adapter.data.access.test;
+
+import cm.xenonbyte.farmbyte.catalog.domain.core.product.Product;
+import cm.xenonbyte.farmbyte.catalog.domain.core.product.ProductCategoryId;
+import cm.xenonbyte.farmbyte.catalog.domain.core.product.ProductType;
+import cm.xenonbyte.farmbyte.catalog.domain.core.product.ports.secondary.ProductRepository;
+import cm.xenonbyte.farmbyte.common.domain.vo.Name;
+import cm.xenonbyte.farmbyte.common.domain.vo.Text;
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author bamk
+ * @version 1.0
+ * @since 23/08/2024
+ */
+public abstract class ProductRepositoryTest {
+
+    protected ProductRepository productRepository;
+    protected Name name;
+
+    @Test
+    protected void should_return_false_when_find_product_with_an_un_existing_name() {
+        //Act
+        Boolean result = productRepository.existsByName(Name.of(Text.of("Product.1")));
+
+        //Then
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    protected void should_return_true_when_find_product_with_an_existing_name() {
+        //Act
+        Boolean result = productRepository.existsByName(name);
+
+        //Then
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    protected void should_create_product() {
+        //Given
+        Name name1 = Name.of(Text.of("Product.3"));
+        Product product = Product.builder()
+                .name(name1)
+                .categoryId(new ProductCategoryId(UUID.randomUUID()))
+                .type(ProductType.SERVICE)
+                .build();
+        product.initiate();
+
+        //Act
+        Product result = productRepository.save(product);
+        Boolean isExistByName = productRepository.existsByName(name1);
+
+        //Then
+        assertThat(result)
+                .isNotNull()
+                .isEqualTo(product);
+        assertThat(isExistByName).isTrue();
+    }
+}
