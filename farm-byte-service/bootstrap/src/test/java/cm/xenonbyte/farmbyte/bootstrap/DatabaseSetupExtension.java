7b4cbd14f1da57622c3a7a9a76de11b085bfe280
Brice MBIANDJI <ambiandji@gmail.com>
2024-08-28 14:24:47 +0200

    FB-10 | Service | Adding Integration test for create uom category

---
+++ farm-byte-service/bootstrap/src/test/java/cm/xenonbyte/farmbyte/bootstrap/DatabaseSetupExtension.java
@@ -0,0 +1,31 @@
+package cm.xenonbyte.farmbyte.bootstrap;
+
+import org.junit.jupiter.api.extension.AfterAllCallback;
+import org.junit.jupiter.api.extension.BeforeAllCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
+
+/**
+ * @author bamk
+ * @version 1.0
+ * @since 26/08/2024
+ */
+public final class DatabaseSetupExtension implements BeforeAllCallback, AfterAllCallback {
+    @Override
+    public void beforeAll(ExtensionContext context) throws Exception {
+        PostgresContainer.container.start();
+        updateDataSourceProps(PostgresContainer.container);
+    }
+
+    @Override
+    public void afterAll(ExtensionContext context) throws Exception {
+        PostgresContainer.container.stop();
+    }
+
+    private void updateDataSourceProps(PostgresContainer container) {
+        System.setProperty("spring.datasource.url", container.getJdbcUrl());
+        System.setProperty("spring.datasource.username", container.getUsername());
+        System.setProperty("spring.datasource.password", container.getPassword());
+    }
+
+
+}
