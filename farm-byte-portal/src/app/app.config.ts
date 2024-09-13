a830d5a8f02f4eedf2b7054eb8d6c83642200678
xenonbytesarl <xenonbytesarl@gmail.com>
2024-09-13 10:39:55 +0200

    FB-8 | Portal | finish implement layout

--- farm-byte-portal/src/app/app.config.ts
+++ farm-byte-portal/src/app/app.config.ts
@@ -1,11 +1,26 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {ApplicationConfig, importProvidersFrom, provideZoneChangeDetection} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import {HttpClient, provideHttpClient} from "@angular/common/http";
+import {TranslateHttpLoader} from "@ngx-translate/http-loader";
+import {TranslateLoader, TranslateModule} from "@ngx-translate/core";
+
+const httpLoaderFactory: (http: HttpClient) => TranslateHttpLoader = (http: HttpClient) =>
+  new TranslateHttpLoader(http, './i18n/', '.json');
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideHttpClient(),
+    provideRouter(routes),
+    importProvidersFrom([TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: httpLoaderFactory,
+        deps: [HttpClient],
+      },
+    })])
   ]
 };
--- farm-byte-portal/src/app/layout/navbar/navbar.component.ts
+++ farm-byte-portal/src/app/layout/navbar/navbar.component.ts
@@ -1,13 +1,17 @@
 import {Component} from '@angular/core';
 import {NavbarModuleComponent} from "./navbar-module/navbar-module.component";
 import {NavbarMenuComponent} from "./navbar-menu/navbar-menu.component";
+import {NavbarLanguageComponent} from "./navbar-language/navbar-language.component";
+import {NavbarProfileComponent} from "./navbar-profile/navbar-profile.component";
 
 @Component({
   selector: 'farmbyte-navbar',
   standalone: true,
   imports: [
     NavbarModuleComponent,
-    NavbarMenuComponent
+    NavbarMenuComponent,
+    NavbarLanguageComponent,
+    NavbarProfileComponent
   ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
