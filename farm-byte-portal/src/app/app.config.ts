9ce3e0a4e782899716ada67ca2a4733c8256423c
xenonbytesarl <xenonbytesarl@gmail.com>
2024-09-14 13:49:40 +0200

    FB-19 | Portal | add configuration service for environment

--- farm-byte-portal/src/app/app.config.ts
+++ farm-byte-portal/src/app/app.config.ts
@@ -1,13 +1,17 @@
-import {ApplicationConfig, importProvidersFrom, provideZoneChangeDetection} from '@angular/core';
+import {APP_INITIALIZER, ApplicationConfig, importProvidersFrom, provideZoneChangeDetection} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import {HttpClient, provideHttpClient} from "@angular/common/http";
 import {TranslateHttpLoader} from "@ngx-translate/http-loader";
 import {TranslateLoader, TranslateModule} from "@ngx-translate/core";
+import {ConfigInitService} from "./core/config-init.service";
+import {take} from "rxjs";
 
 const httpLoaderFactory: (http: HttpClient) => TranslateHttpLoader = (http: HttpClient) =>
-  new TranslateHttpLoader(http, './i18n/', '.json');
+  new TranslateHttpLoader(http, './assets/i18n/', '.json');
+
+const configInit: () => ConfigInitService = () => new ConfigInitService();
 
 
 export const appConfig: ApplicationConfig = {
@@ -15,12 +19,24 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideHttpClient(),
     provideRouter(routes),
-    importProvidersFrom([TranslateModule.forRoot({
-      loader: {
-        provide: TranslateLoader,
-        useFactory: httpLoaderFactory,
-        deps: [HttpClient],
+    importProvidersFrom([
+      TranslateModule.forRoot({
+        loader: {
+          provide: TranslateLoader,
+          useFactory: httpLoaderFactory,
+          deps: [HttpClient],
+        },
+      })
+    ]),
+    {
+      provide: APP_INITIALIZER,
+      useFactory: (configInit: ConfigInitService) => {
+        return () => {
+          return configInit.loadConfig().pipe(take(1));
+        }
       },
-    })])
+      multi: true,
+      deps: [ConfigInitService],
+    }
   ]
 };
