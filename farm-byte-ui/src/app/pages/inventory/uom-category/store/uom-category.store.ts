05d401bc8f76af5a4f6c78fd55084a1f76ca3ce9
xenonbytesarl <xenonbytesarl@gmail.com>
2024-09-21 00:16:10 +0200

    FB-19 | Portal | Add uom category form and implement create, update and find uom by adding.

--- farm-byte-ui/src/app/pages/inventory/uom-category/store/uom-category.store.ts
+++ farm-byte-ui/src/app/pages/inventory/uom-category/store/uom-category.store.ts
@@ -10,21 +10,28 @@ import {FindParamModel} from "../../../../core/model/find-param.model";
 import {SuccessResponseModel} from "../../../../core/model/success-response.model";
 import {withDevtools} from "@angular-architects/ngrx-toolkit";
 import {SearchParamModel} from "../../../../core/model/search-param.model";
-import {setAllEntities, withEntities} from "@ngrx/signals/entities";
+import {addEntity, setAllEntities, setEntity, updateEntity, withEntities} from "@ngrx/signals/entities";
 import {DEBOUNCE_TIMEOUT} from "../../../../core/constants/app.constant";
+import {MessageService} from "primeng/api";
+import {FormMode} from "../../../../core/enums/form-mode.enum";
+import {Router} from "@angular/router";
 
 type UomCategoryState = {
   pageSize: number;
   totalElements: number;
   loading: boolean;
   error: any;
+  formMode: FormMode | undefined,
+  selectedUomCategory: UomCategoryModel | undefined,
 };
 
 const uomCategoryInitialState: UomCategoryState = {
   pageSize: 0,
   totalElements: 0,
   loading: false,
-  error: null
+  error: null,
+  formMode: undefined,
+  selectedUomCategory: undefined,
 };
 
 
@@ -33,7 +40,10 @@ export const UomCategoryStore = signalStore(
   withState(uomCategoryInitialState),
   withEntities({ entity: type<UomCategoryModel>(), collection: 'uomCategory' }),
   withDevtools('uomCategoryState'),
-  withMethods((store, uomCategoryService = inject(UomCategoryService)) => ({
+  withMethods((store,
+               uomCategoryService = inject(UomCategoryService),
+               messageService = inject(MessageService),
+               router = inject(Router)) => ({
     findUomCategories: rxMethod<FindParamModel>(
       pipe(
         tap(() => patchState(store, (state) => ({...state, loading: true}))),
@@ -90,8 +100,93 @@ export const UomCategoryStore = signalStore(
         )
       )
     ),
-    findUomCategoryById(id: string): WritableSignal<UomCategoryModel | undefined> {
-      return signal(store.uomCategoryEntities().find((uomCategory: UomCategoryModel) => uomCategory.id === id));
+    findUomCategoryById: rxMethod<string>(
+      pipe(
+        tap(() => patchState(store, (state) => ({...state, loading: true}))),
+        switchMap((uomCategoryId: string) =>
+          uomCategoryService.findUomCategoryById$(uomCategoryId)
+            .pipe(
+              tapResponse({
+                next: (uomCategorySuccessResponse: SuccessResponseModel<UomCategoryModel>) => {
+                  patchState(
+                    store,
+                    (state) => ({
+                      ...state,
+                      selectedUomCategory: uomCategorySuccessResponse.data.content
+                    })
+                  );
+                },
+                error: (error) => {
+                  patchState(store, state => ({...state, error}));
+                },
+                finalize: () => patchState(store, (state) => ({...state, loading: false}))
+              })
+            )
+          )
+        )
+      ),
+    createUomCategory: rxMethod<UomCategoryModel>(
+      pipe(
+        tap(() => patchState(store, (state) => ({...state, loading: true}))),
+        switchMap((uomCategory: UomCategoryModel) =>
+          uomCategoryService.createUomCategory$(uomCategory)
+            .pipe(
+              tapResponse({
+                next: (uomCategorySuccessResponse: SuccessResponseModel<UomCategoryModel>) => {
+                  patchState(
+                    store,
+                    addEntity(uomCategorySuccessResponse.data.content, {collection: 'uomCategory'}),
+                    (state) => ({
+                      ...state,
+                      totalElements: state.totalElements + 1
+                    })
+                  );
+                  messageService.add({severity: 'success', summary: 'Info', detail: uomCategorySuccessResponse.message});
+                  router.navigate([`/inventory/uom-categories/details/${uomCategorySuccessResponse.data.content.id}`]);
+                },
+                error: (error) => {
+                  patchState(store, state => ({...state, error}));
+                  messageService.add({severity: 'error', summary: 'Info', detail: error as string});
+                },
+                finalize: () => patchState(store, (state) => ({...state, loading: false}))
+              })
+            )
+        )
+      )
+    ),
+    updateUomCategory: rxMethod<UomCategoryModel>(
+      pipe(
+        tap(() => patchState(store, (state) => ({...state, loading: true}))),
+        switchMap((uomCategory: UomCategoryModel) =>
+          uomCategoryService.updateUomCategory$(uomCategory)
+            .pipe(
+              tapResponse({
+                next: (uomCategorySuccessResponse: SuccessResponseModel<UomCategoryModel>) => {
+                  patchState(
+                    store,
+                    setEntity(uomCategorySuccessResponse.data.content, {collection: 'uomCategory'}),
+                    (state => ({
+                      formMode: FormMode.READ,
+                      selectedUomCategory: uomCategorySuccessResponse.data.content
+                    }))
+                  );
+                  messageService.add({severity: 'info', summary: 'Info', detail: uomCategorySuccessResponse.message});
+                },
+                error: (error) => {
+                  patchState(store, state => ({...state, error}));
+                  messageService.add({severity: 'error', summary: 'Info', detail: error as string});
+                },
+                finalize: () => patchState(store, (state) => ({...state, loading: false}))
+              })
+            )
+        )
+      )
+    ),
+    initForm(mode: FormMode): void {
+      patchState(store, state => ({...state, formMode: mode}))
+    },
+    clearSelectedUomCategory(): void {
+      patchState(store, state => ({...state, selectedUomCategory: undefined}))
     }
   }))
 );
