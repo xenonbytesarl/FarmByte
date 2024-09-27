d99dc29681117c4232e373c6e676bc11afe16b3b
xenonbytesarl <xenonbytesarl@gmail.com>
2024-09-28 01:20:40 +0200

    FB-19 | Portal | add sliding navbar and array of menu in navbar and refactoring model menu

---
+++ farm-byte-fronend/src/layouts/model/sidebar-menu.ts
@@ -0,0 +1,8 @@
+import {NavbarMenuModel} from "@/layouts/model/navbar-menu-model.ts";
+
+export interface SidebarMenuModel {
+    label: string;
+    icon: string;
+    link: string;
+    navbarMenu?: NavbarMenuModel;
+}
