a99d50a8fb736156e163406be7b7283fca95b44e
xenonbytesarl <xenonbytesarl@gmail.com>
2024-09-28 01:20:40 +0200

    FB-19 | Portal | add sliding navbar and array of menu in navbar and refactoring model menu

--- farm-byte-fronend/src/layouts/sidebar/Sidebar.tsx
+++ farm-byte-fronend/src/layouts/sidebar/Sidebar.tsx
@@ -1,57 +1,38 @@
-
+import {useState} from "react";
+import {SidebarMenuModel} from "@/layouts/model/sidebar-menu.ts";
+import {sidebarMenus} from "@/layouts/data/sidebar-menu-data.ts";
 
 const Sidebar = () => {
+    const [isOpen, setIsOpen] = useState(true);
+
+    const toggleSidebar =  () => {
+        setIsOpen(!isOpen);
+    }
     return (
-        <>
-            <div className="flex flex-col bg-amber-800 min-h-screen py-3 w-72 text-white">
-                <div className="flex flex-row justify-between items-center px-3 mb-5 w-full">
-                    <div className=" flex flex-row justify-between items-center w-[95%]">
-                        <p className="text-2xl font-medium">FarmByte.cm</p>
-                        <span className="material-symbols-outlined cursor-pointer rounded-full">menu</span>
-                    </div>
+        <div className={`flex flex-col bg-amber-800 min-h-screen py-3 text-white ${isOpen? 'w-80 transition-all ease-linear duration-300': 'w-20 transition-all ease-linear duration-300'}`}>
+            <div className="relative flex flex-row justify-between items-center px-5 mb-5 w-full">
+                <div className={`flex flex-row justify-between items-center w-[95%]`}>
+                    <p className={` ${isOpen ? 'block text-xl font-medium' : 'hidden text-2xl text-center'}`}>FarmByte.cm</p>
                 </div>
-                <div className="flex flex-col justify-start items-start text-xl">
-                    <div
-                        className="flex flex-row justify-start items-center gap-3 px-3 py-5 cursor-pointer w-full text-white hover:bg-amber-900/40 hover:text-amber-500 hover:transition hover:duration-300 hover:ease-in-out">
-                        <span className="material-symbols-outlined">dashboard</span>
-                        <p>Dashboard</p>
-                    </div>
-                    <div
-                        className="flex flex-row justify-start items-center gap-3 px-3 py-5 cursor-pointer w-full text-white hover:bg-amber-900/40 hover:text-amber-500 hover:transition hover:duration-300 hover:ease-in-out">
-                        <span className="material-symbols-outlined">inventory</span>
-                        <p>Inventory</p>
-                    </div>
-                    <div
-                        className="flex flex-row justify-start items-center gap-3 px-3 py-5 cursor-pointer w-full text-white hover:bg-amber-900/40 hover:text-amber-500 hover:transition hover:duration-300 hover:ease-in-out">
-                        <span className="material-symbols-outlined">storefront</span>
-                        <p>Sale</p>
-                    </div>
-                    <div
-                        className="flex flex-row justify-start items-center gap-3 px-3 py-5 cursor-pointer w-full text-white hover:bg-amber-900/40 hover:text-amber-500 hover:transition hover:duration-300 hover:ease-in-out">
-                        <span className="material-symbols-outlined">shopping_bag</span>
-                        <p>Purchase</p>
-                    </div>
-                    <div
-                        className="flex flex-row justify-start items-center gap-3 px-3 py-5 cursor-pointer w-full text-white hover:bg-amber-900/40 hover:text-amber-500 hover:transition hover:duration-300 hover:ease-in-out">
-                        <span className="material-symbols-outlined">receipt</span>
-                        <p>Invoice</p>
-                    </div>
-                    <div
-                        className="flex flex-row justify-start items-center gap-3 px-3 py-5 cursor-pointer w-full text-white hover:bg-amber-900/40 hover:text-amber-500 hover:transition hover:duration-300 hover:ease-in-out">
-                        <span className="material-symbols-outlined">agriculture</span>
-                        <p>Farm</p>
-                    </div>
-                    <div
-                        className="flex flex-row justify-start items-center gap-3 px-3 py-5 cursor-pointer w-full text-white hover:bg-amber-900/40 hover:text-amber-500 hover:transition hover:duration-300 hover:ease-in-out">
-                        <span className="material-symbols-outlined">settings</span>
-                        <p>Setting</p>
-                    </div>
-                </div>
-                <div className="flex flex-row justify-center items-end">
-                    <span>copyright	&#169;2024</span>
-                </div>
+                <p onClick={toggleSidebar}
+                   className={`absolute -right-3.5 material-symbols-outlined text-white cursor-pointer p-1 bg-amber-600 rounded-full ${isOpen ? '' : 'mt-8'}`}>{isOpen ? 'arrow_back' : 'arrow_forward'}</p>
             </div>
-        </>
+            <div className="flex flex-col justify-start items-start text-lg">
+                {
+                    sidebarMenus.map((sidebarMenu : SidebarMenuModel) => (
+                        <div
+                            key={sidebarMenu.label}
+                            className={`flex flex-row ${isOpen? 'justify-start': 'justify-center'} items-center gap-3 px-3 py-5 cursor-pointer w-full text-white hover:bg-amber-900/40 hover:text-amber-500 hover:transition hover:duration-300 hover:ease-in-out`}>
+                            <span className="material-symbols-outlined">{sidebarMenu.icon}</span>
+                            <p className={`${isOpen? 'transition-all ease-out duration-300 opacity-100': 'hidden transition-all ease-in duration-300 opacity-0'}`}>{sidebarMenu.label}</p>
+                        </div>
+                    ))
+                }
+            </div>
+            <div className={`flex flex-row justify-center items-end mt-auto mx-auto border-t-2 py-5 w-[95%] border-amber-700 ${isOpen? 'block': 'hidden'}`}>
+                <span>copyright    &#169;2024</span>
+            </div>
+        </div>
     );
 }
 
