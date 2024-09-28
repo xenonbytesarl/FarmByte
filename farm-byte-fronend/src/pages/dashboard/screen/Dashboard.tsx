861a6d0e703056654fc67144e73245d0a4f2f517
xenonbytesarl <xenonbytesarl@gmail.com>
2024-09-28 15:20:15 +0200

    FB-19 | Portal | add configuration for environment

--- farm-byte-fronend/src/pages/dashboard/screen/Dashboard.tsx
+++ farm-byte-fronend/src/pages/dashboard/screen/Dashboard.tsx
@@ -1,6 +1,12 @@
+
 const Dashboard = () => {
     return (
-        <div className="text-3xl text-amber-700">DASHBOARD</div>
+        <div className="text-3xl text-amber-700">
+            DASHBOARD<br/>
+            {import.meta.env.VITE_API_BACKEND_URL}
+            <br/>
+            {import.meta.env.VITE_APP_TITLE}
+        </div>
     );
 }
 
