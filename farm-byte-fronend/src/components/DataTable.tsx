70eed83eb293e259aa74f440994f9f43698e41ff
xenonbytesarl <xenonbytesarl@gmail.com>
2024-10-05 06:19:24 +0200

    FB-19 | Portal | implement uom tree

--- farm-byte-fronend/src/components/DataTable.tsx
+++ farm-byte-fronend/src/components/DataTable.tsx
@@ -11,6 +11,7 @@ import {Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle} f
 import {useTranslation} from "react-i18next";
 import {useState} from "react";
 import {Input} from "@/components/ui/input.tsx";
+import {DEFAULT_PAGE_SIZE_OPTIONS} from "@/constants/page.constant.ts";
 
 
 
@@ -24,14 +25,15 @@ interface DataTableProps<TData, TValue> {
     isLoading?: boolean,
     title: string,
     keyword: string,
-    handlePaginatorChange: (page: number, size: number) => void,
-    handleNew: (link: string) => void,
-    handleFilter: (keyword: string) => void,
+    handlePageChange: (page: number) => void,
+    handleSizeChange: (size: number) => void,
+    handleNew: () => void,
+    handleFilterChange: (keyword: string) => void,
     handleClear: () => void,
 }
 
 const  DataTable = <TData, TValue>({columns, data, totalElements, totalPages, page, size, title,
-            keyword, handlePaginatorChange, handleNew,  handleFilter, handleClear}: DataTableProps<TData, TValue>) => {
+            keyword, handlePageChange, isLoading, handleSizeChange, handleNew,  handleFilterChange, handleClear}: DataTableProps<TData, TValue>) => {
 
     const {t} = useTranslation(['home']);
 
@@ -63,7 +65,7 @@ const  DataTable = <TData, TValue>({columns, data, totalElements, totalPages, pa
                 <CardHeader>
                     <CardTitle className=" flex flex-row justify-between items-center text-amber-700">
                         <p className="text-2xl">{t(title)}</p>
-                        <Button onClick={() => handleNew} variant="default" className="flex flex-row justify-center items-center">
+                        <Button onClick={() => handleNew()} variant="default" className="flex flex-row justify-center items-center">
                             <span className="material-symbols-outlined text-xl">add</span>
                             <span>{t('tree_button_label_add')}</span>
                         </Button>
@@ -73,7 +75,7 @@ const  DataTable = <TData, TValue>({columns, data, totalElements, totalPages, pa
                             <Input
                                 placeholder={t('tree_search_place_holder')}
                                 value={keyword}
-                                onChange={(event) => handleFilter(event.target.value)}
+                                onChange={(event) => handleFilterChange(event.target.value)}
                                 className="max-w-sm"
                             />
                             <span onClick={() => handleClear()} className={`material-symbols-outlined absolute mr-2 cursor-pointer text-2xl}`}>{keyword.length > 0? 'close': 'search'}</span>
@@ -103,83 +105,93 @@ const  DataTable = <TData, TValue>({columns, data, totalElements, totalPages, pa
                             </TableHeader>
                             <TableBody>
                                 {
-                                    table.getRowModel().rows?.length ? (
-                                        table.getRowModel().rows.map((row) => (
-                                            <TableRow
-                                                /*onClick={() => handleEdit(row.original)}*/
-                                                key={row.id}
-                                                data-state={row.getIsSelected() && "selected"}
-                                                className={row.getIsSelected() ? 'selected' : null}
-                                                onClick={row.getToggleSelectedHandler()}
-                                            >
-                                                {row.getVisibleCells().map((cell) => (
-                                                    <TableCell key={cell.id}>
-                                                        {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                                    isLoading
+                                        ?
+                                            <p className="flex flex-row justify-center items-center w-full mt-8">'Loading...'</p>
+                                        :
+                                            table.getRowModel().rows?.length ? (
+                                                table.getRowModel().rows.map((row) => (
+                                                    <TableRow
+                                                        /*onClick={() => handleEdit(row.original)}*/
+                                                        key={row.id}
+                                                        data-state={row.getIsSelected() && "selected"}
+                                                        className={row.getIsSelected() ? 'selected' : null}
+                                                        onClick={row.getToggleSelectedHandler()}
+                                                    >
+                                                        {row.getVisibleCells().map((cell) => (
+                                                            <TableCell key={cell.id}>
+                                                                {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                                                            </TableCell>
+                                                        ))}
+                                                    </TableRow>
+                                                ))
+                                            ) : (
+                                                <TableRow>
+                                                    <TableCell colSpan={columns.length} className="h-24 text-center">
+                                                        No results.
                                                     </TableCell>
-                                                ))}
-                                            </TableRow>
-                                        ))
-                                    ) : (
-                                        <TableRow>
-                                            <TableCell colSpan={columns.length} className="h-24 text-center">
-                                                No results.
-                                            </TableCell>
-                                        </TableRow>
-                                    )
+                                                </TableRow>
+                                            )
                                 }
                             </TableBody>
                         </Table>
                     </div>
                 </CardContent>
                 <CardFooter className="flex items-center justify-between space-x-2 py-4">
-                        <select
-                            className="text-base w-16 text-center"
-                            value={table.getState().pagination.pageSize}
-                            onChange={event => handlePaginatorChange(table.getState().pagination.pageIndex, Number(event.target.value))}
-                        >
-                            {[2, 3, 5, 10, 100].map(opt => (
-                                <option key={opt} value={opt}>
-                                    {opt}
-                                </option>
-                            ))}
-                        </select>
-                        <div className="flex flex-row justify-end items-center">
-                            <Button
-                                variant="outline"
-                                size="sm"
-                                onClick={() => handlePaginatorChange(0, table.getState().pagination.pageSize)}
-                                disabled={!table.getCanPreviousPage()}
+                    {
+                        !isLoading
+                            &&
+                        <div className="w-full">
+                            <select
+                                className="text-base w-16 text-center"
+                                value={table.getState().pagination.pageSize}
+                                onChange={event => handleSizeChange(Number(event.target.value))}
                             >
-                                {'<<'}
-                            </Button>
-                            <Button
-                                variant="outline"
-                                size="sm"
-                                onClick={() => handlePaginatorChange(table.getState().pagination.pageIndex - 1, table.getState().pagination.pageSize)}
-                                disabled={!table.getCanPreviousPage()}
-                            >
-                                {'<'}
-                            </Button>
-                            <Button
-                                variant="outline"
-                                size="sm"
-                                onClick={() => handlePaginatorChange(table.getState().pagination.pageIndex + 1, table.getState().pagination.pageSize)}
-                                disabled={!table.getCanNextPage()}
-                            >
-                                {'>'}
-                            </Button>
-                            <Button
-                                variant="outline"
-                                size="sm"
-                                onClick={() => handlePaginatorChange(totalPages - 1, table.getState().pagination.pageSize)}
-                                disabled={!table.getCanNextPage()}
-                            >
-                                {'>>'}
-                            </Button>
-                            <p className="text-sm ml-8">
-                                {page == 0 ? size > totalElements ? totalElements : size : (page + 1) == totalPages ? totalElements : (page + 1) * size} of {totalElements}
-                            </p>
+                                {DEFAULT_PAGE_SIZE_OPTIONS.map(opt => (
+                                    <option key={opt} value={opt}>
+                                        {opt}
+                                    </option>
+                                ))}
+                            </select>
+                            <div className="flex flex-row justify-end items-center">
+                                <Button
+                                    variant="outline"
+                                    size="sm"
+                                    onClick={() => handlePageChange(0)}
+                                    disabled={!table.getCanPreviousPage()}
+                                >
+                                    {'<<'}
+                                </Button>
+                                <Button
+                                    variant="outline"
+                                    size="sm"
+                                    onClick={() => handlePageChange(table.getState().pagination.pageIndex - 1)}
+                                    disabled={!table.getCanPreviousPage()}
+                                >
+                                    {'<'}
+                                </Button>
+                                <Button
+                                    variant="outline"
+                                    size="sm"
+                                    onClick={() => handlePageChange(table.getState().pagination.pageIndex + 1)}
+                                    disabled={!table.getCanNextPage()}
+                                >
+                                    {'>'}
+                                </Button>
+                                <Button
+                                    variant="outline"
+                                    size="sm"
+                                    onClick={() => handlePageChange(totalPages - 1)}
+                                    disabled={!table.getCanNextPage()}
+                                >
+                                    {'>>'}
+                                </Button>
+                                <p className="text-sm ml-8">
+                                    {page == 0 ? size > totalElements ? totalElements : size : (page + 1) == totalPages ? totalElements : (page + 1) * size} of {totalElements}
+                                </p>
+                            </div>
                         </div>
+                    }
                 </CardFooter>
             </Card>
 
