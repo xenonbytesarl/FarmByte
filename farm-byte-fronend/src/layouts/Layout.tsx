48c9e636bca4d6cc735181f160e1e2b33026f7b5
xenonbytesarl <xenonbytesarl@gmail.com>
2024-10-07 02:08:54 +0200

    FB-19 | Portal | improve layout to fix nav and sidebar

--- farm-byte-fronend/src/layouts/Layout.tsx
+++ farm-byte-fronend/src/layouts/Layout.tsx
@@ -2,18 +2,21 @@ import Sidebar from "@/layouts/sidebar/Sidebar.tsx";
 import WebNavbar from "@/layouts/navbar/web-navbar/WebNavbar.tsx";
 import MobileNavbar from "@/layouts/navbar/mobile-navbar/MobileNavbar.tsx";
 import {Outlet} from "react-router-dom";
+import {useSelector} from "react-redux";
+import {selectIsOpenSidebarMenu} from "@/layouts/sidebar/SidebarMenuSlice.ts";
 
 
 
 const Layout = () => {
+    const isOpen: boolean = useSelector(selectIsOpenSidebarMenu);
     return (
-        <div className="flex flex-row min-h-screen bg-neutral-50">
+        <div className="flex flex-row min-h-screen bg-gradient-to-r from-neutral-50 to-neutral-100">
             <div className="hidden md:block">
                 <div className="flex flex-row w-screen">
                     <Sidebar/>
                     <div className="flex flex-col w-full">
                         <WebNavbar/>
-                        <div className="container grow mx-auto min-h-full flex-1 p-10">
+                        <div className={`min-h-full flex-1 p-10 ${isOpen ? 'ml-72 transition-all ease-linear duration-300' : 'ml-24 transition-all ease-linear duration-300'}`}>
                             <Outlet/>
                         </div>
                     </div>
