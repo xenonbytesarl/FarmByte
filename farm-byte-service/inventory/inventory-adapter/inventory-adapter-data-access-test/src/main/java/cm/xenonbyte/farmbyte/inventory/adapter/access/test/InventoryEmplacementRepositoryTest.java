c8ffd5d081d20feefcde4d5de66a5d1d6c52d160
xenonbytesarl <xenonbytesarl@gmail.com>
2024-10-19 22:27:43 +0200

    FB-26 | Service | implement search inventory emplacements by keyword in inmemory adapter

--- farm-byte-service/inventory/inventory-adapter/inventory-adapter-data-access-test/src/main/java/cm/xenonbyte/farmbyte/inventory/adapter/access/test/InventoryEmplacementRepositoryTest.java
+++ farm-byte-service/inventory/inventory-adapter/inventory-adapter-data-access-test/src/main/java/cm/xenonbyte/farmbyte/inventory/adapter/access/test/InventoryEmplacementRepositoryTest.java
@@ -1,6 +1,9 @@
 package cm.xenonbyte.farmbyte.inventory.adapter.access.test;
 
+import cm.xenonbyte.farmbyte.common.domain.vo.Direction;
+import cm.xenonbyte.farmbyte.common.domain.vo.Keyword;
 import cm.xenonbyte.farmbyte.common.domain.vo.Name;
+import cm.xenonbyte.farmbyte.common.domain.vo.PageInfo;
 import cm.xenonbyte.farmbyte.common.domain.vo.Text;
 import cm.xenonbyte.farmbyte.inventory.domain.core.inventoryemplacement.InventoryEmplacement;
 import cm.xenonbyte.farmbyte.inventory.domain.core.inventoryemplacement.InventoryEmplacementId;
@@ -8,6 +11,7 @@ import cm.xenonbyte.farmbyte.inventory.domain.core.inventoryemplacement.Inventor
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -23,6 +27,7 @@ public abstract class InventoryEmplacementRepositoryTest {
     protected Name name;
     protected InventoryEmplacementId parentId;
     protected InventoryEmplacement inventoryEmplacement;
+    protected InventoryEmplacementId inventoryEmplacementId;
 
     @Nested
     class CreateInventoryEmplacementRepositoryTest {
@@ -68,6 +73,94 @@ public abstract class InventoryEmplacementRepositoryTest {
 
             //Then
             assertThat(result).isNotNull().isEqualTo(inventoryEmplacement);
+        }
+    }
+
+    @Nested
+    class FindInventoryEmplacementByIdRepositoryTest {
+
+        @Test
+        void should_success_when_find_inventory_emplacement_by_id() {
+            //Given + Act
+            Optional<InventoryEmplacement> result = inventoryEmplacementRepository.findById(inventoryEmplacementId);
+
+            //Then
+            assertThat(result.isPresent()).isTrue();
+        }
+
+        @Test
+        void should_fail_when_find_inventory_emplacement_by_id() {
+            //Given + Act
+            Optional<InventoryEmplacement> result =
+                    inventoryEmplacementRepository.findById(new InventoryEmplacementId(UUID.fromString("0192a66b-2ca4-771f-8b70-d57d4b66f797")));
+
+            //Then
+            assertThat(result.isPresent()).isFalse();
+        }
+    }
+
+    @Nested
+    class FindInventoryEmplacementsRepositoryTest {
+        @Test
+        void should_success_when_find_inventory_emplacements() {
+            //Given
+            Integer page = 0;
+            Integer size = 2;
+            String sortAttribute = "name";
+            Direction direction = Direction.ASC;
+
+            //Act
+            PageInfo<InventoryEmplacement> result =
+                    inventoryEmplacementRepository.findAll(page, size, sortAttribute, direction);
+
+            //Then
+            assertThat(result.getTotalElements()).isGreaterThan(0);
+            assertThat(result.getTotalPages()).isGreaterThan(0);
+            assertThat(result.getElements().size()).isGreaterThan(0);
+            assertThat(result.getTotalPages()).isGreaterThan(0);
+        }
+    }
+
+    @Nested
+    class SearchInventoryEmplacementsRepositoryTest {
+        @Test
+        void should_success_when_find_inventory_emplacements() {
+            //Given
+            Integer page = 0;
+            Integer size = 2;
+            String sortAttribute = "name";
+            Direction direction = Direction.ASC;
+            Keyword keyword = Keyword.of(Text.of("int"));
+
+            //Act
+            PageInfo<InventoryEmplacement> result =
+                    inventoryEmplacementRepository.search(page, size, sortAttribute, direction, keyword);
+
+            //Then
+            assertThat(result.getTotalElements()).isGreaterThan(0);
+            assertThat(result.getTotalPages()).isGreaterThan(0);
+            assertThat(result.getElements().size()).isGreaterThan(0);
+            assertThat(result.getTotalPages()).isGreaterThan(0);
+        }
+
+        @Test
+        void should_empty_when_find_inventory_emplacements() {
+            //Given
+            Integer page = 0;
+            Integer size = 2;
+            String sortAttribute = "name";
+            Direction direction = Direction.ASC;
+            Keyword keyword = Keyword.of(Text.of("xxxx"));
+
+            //Act
+            PageInfo<InventoryEmplacement> result =
+                    inventoryEmplacementRepository.search(page, size, sortAttribute, direction, keyword);
+
+            //Then
+            assertThat(result.getTotalElements()).isEqualTo(0);
+            assertThat(result.getTotalPages()).isEqualTo(0);
+            assertThat(result.getElements().size()).isEqualTo(0);
+            assertThat(result.getTotalPages()).isEqualTo(0);
         }
     }
 }
