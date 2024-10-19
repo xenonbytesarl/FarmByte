d327487a79e3679d74f9e6d17e234885c8e7ff92
xenonbytesarl <xenonbytesarl@gmail.com>
2024-10-19 22:51:35 +0200

    FB-26 | Service | implement search inventory emplacements by keyword in jpa adapter

--- farm-byte-service/inventory/inventory-adapter/inventory-adapter-data-access-jpa/src/main/java/cm/xenonbyte/farmbyte/inventory/adapter/data/access/jpa/InventoryEmplacementJpaRepositoryAdapter.java
+++ farm-byte-service/inventory/inventory-adapter/inventory-adapter-data-access-jpa/src/main/java/cm/xenonbyte/farmbyte/inventory/adapter/data/access/jpa/InventoryEmplacementJpaRepositoryAdapter.java
@@ -9,6 +9,9 @@ import cm.xenonbyte.farmbyte.inventory.domain.core.inventoryemplacement.Inventor
 import cm.xenonbyte.farmbyte.inventory.domain.core.inventoryemplacement.InventoryEmplacementRepository;
 import jakarta.annotation.Nonnull;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -61,16 +64,37 @@ public class InventoryEmplacementJpaRepositoryAdapter implements InventoryEmplac
     @Nonnull
     @Override
     public Optional<InventoryEmplacement> findById(@Nonnull InventoryEmplacementId inventoryEmplacementId) {
-        return Optional.empty();
+        return inventoryEmplacementRepositoryJpa.findById(inventoryEmplacementId.getValue())
+                .map(inventoryEmplacementJpaMapper::toInventoryEmplacement);
     }
 
     @Override
     public PageInfo<InventoryEmplacement> findAll(Integer page, Integer size, String sortAttribute, Direction direction) {
-        return null;
+        Sort.Direction sortDirection = Direction.ASC.equals(direction) ? Sort.Direction.ASC : Sort.Direction.DESC;
+        Page<InventoryEmplacementJpa> inventoryEmplacementJpaPage =
+                inventoryEmplacementRepositoryJpa.findAll(PageRequest.of(page, size, sortDirection, sortAttribute));
+        return new PageInfo<>(
+                inventoryEmplacementJpaPage.isFirst(),
+                inventoryEmplacementJpaPage.isLast(),
+                inventoryEmplacementJpaPage.getSize(),
+                inventoryEmplacementJpaPage.getTotalElements(),
+                inventoryEmplacementJpaPage.getTotalPages(),
+                inventoryEmplacementJpaPage.getContent().stream().map(inventoryEmplacementJpaMapper::toInventoryEmplacement).toList()
+        );
     }
 
     @Override
     public PageInfo<InventoryEmplacement> search(Integer page, Integer size, String sortAttribute, Direction direction, Keyword keyword) {
-        return null;
+        Sort.Direction sortDirection = Direction.ASC.equals(direction) ? Sort.Direction.ASC : Sort.Direction.DESC;
+        Page<InventoryEmplacementJpa> inventoryEmplacementJpaPage =
+                inventoryEmplacementRepositoryJpa.search(PageRequest.of(page, size, sortDirection, sortAttribute), keyword.getText().getValue());
+        return new PageInfo<>(
+                inventoryEmplacementJpaPage.isFirst(),
+                inventoryEmplacementJpaPage.isLast(),
+                inventoryEmplacementJpaPage.getSize(),
+                inventoryEmplacementJpaPage.getTotalElements(),
+                inventoryEmplacementJpaPage.getTotalPages(),
+                inventoryEmplacementJpaPage.getContent().stream().map(inventoryEmplacementJpaMapper::toInventoryEmplacement).toList()
+        );
     }
 }
--- farm-byte-service/inventory/inventory-adapter/inventory-adapter-data-access-jpa/src/main/java/cm/xenonbyte/farmbyte/inventory/adapter/data/access/jpa/InventoryEmplacementRepositoryJpa.java
+++ farm-byte-service/inventory/inventory-adapter/inventory-adapter-data-access-jpa/src/main/java/cm/xenonbyte/farmbyte/inventory/adapter/data/access/jpa/InventoryEmplacementRepositoryJpa.java
@@ -1,6 +1,10 @@
 package cm.xenonbyte.farmbyte.inventory.adapter.data.access.jpa;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
@@ -15,4 +19,7 @@ public interface InventoryEmplacementRepositoryJpa extends JpaRepository<Invento
     Boolean existsByParentJpa(InventoryEmplacementJpa parentJpa);
 
     Boolean existsByNameIgnoreCase(String name);
+
+    @Query("select ie from InventoryEmplacementJpa ie left join ie.parentJpa iep where lower(concat(ie.name, '', ie.type, '', coalesce(iep.name, ''))) like lower(concat('%', :keyword, '%'))")
+    Page<InventoryEmplacementJpa> search(Pageable pageable, @Param("keyword") String keyword);
 }
